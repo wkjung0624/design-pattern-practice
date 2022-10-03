2280bcc12b6c951be5a0e9f5f0d2c9fc1f1bb3ec
Wankyu Jung <skyship36@gmail.com>
2022-10-04 00:07:40 +0900

    P215. DCL(Double-Checked Locking) 방법으로 멀티스레딩 문제 해결

--- src/ch05/ChocolateBoiler.java
+++ src/ch05/ChocolateBoiler.java
@@ -3,7 +3,7 @@ package ch05;
 public class ChocolateBoiler {
 	private boolean empty;
 	private boolean boiled;
-	private static ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+	private static volatile ChocolateBoiler uniqueInstance;
 
 	private ChocolateBoiler() {
 		empty = true;
@@ -11,6 +11,13 @@ public class ChocolateBoiler {
 	}
 
 	public static ChocolateBoiler getInstance() {
+		if (uniqueInstance == null){		// 인스턴스가 있는지 확인 후 없으면 동기화(15번 라인)된 블록으로 진입
+			synchronized (Singleton.class) {		// 이렇게 사용하면 처음에만 동기화함
+				if(uniqueInstance == null) {
+					uniqueInstance = new ChocolateBoiler();		// 블록에서도 다시 한번 변수가 null 인지 체크한 다음 인스턴스 생성함.
+				}
+			}
+		}
 		return uniqueInstance;
 	}
 
