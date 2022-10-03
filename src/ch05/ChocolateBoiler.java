cecec25eaee686518e25da1851c112cdfa2da5f2
Wankyu Jung <skyship36@gmail.com>
2022-10-03 23:49:59 +0900

    P209. 초콜릿 보일러 코드, Singleton 변환 전

---
+++ src/ch05/ChocolateBoiler.java
@@ -0,0 +1,41 @@
+package ch05;
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+
+	private ChocolateBoiler() {
+		empty = true;
+		 boiled = false;
+	}
+
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+			// 보일러에 우유와 초콜릿을 혼합한 재료를 넣음
+		}
+	}
+
+	public void drain(){
+		if (!isEmpty() && isBoiled()){
+			// 끓인 재료를 다음 단계로 넘김
+			empty = true;
+		}
+	}
+
+	public void boil() {
+		if (!isEmpty() && !isBoiled()) {
+			// 재료를 끓임
+			boiled = true;
+		}
+	}
+
+	public boolean isEmpty(){
+		return empty;
+	}
+
+	public boolean isBoiled(){
+		return boiled;
+	}
+}
